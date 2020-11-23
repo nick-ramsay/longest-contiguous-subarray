bdadfc504439ee88c817f45c323290f2c7eacaf7
nick-ramsay <NickRamsay1989@gmail.com>
2020-11-23 16:35:58 +1100

    Removed unnecessary eligibleArrays array variable to improve performance

--- subarray.js
+++ subarray.js
@@ -1,23 +1,17 @@
 let a = [1, 2, 3];
 let k = 4;
 
-
 const longestSubarray = (a, k) => {
     let maximumLength = 0;
     let subarraySum = 0;
     let currentArray = [];
-    let eligibleArrays = [];
     
     for (let i = 0; i < a.length; i++) {
         subarraySum = a[i];
-        currentArray = [a[i]];
-        //Review the value of each index in parameter "a" array. If the value of any index in array is less than or equal to k,
-        //count it as an eligibleArray by pushing it into eligibleArrays array.
-        if (a[i] <= k) {
-            eligibleArrays.push([a[i]]);
+        currentArray = [a[i]];  
+        if (maximumLength === 0 && a[i] <= k) {
+            maximumLength = 1;
         }
-        //Begin adding subsequent indexes in array until sum is greater than or equal to k. Add values of these indexes to "currentArray". 
-        //Once the sum of the currentArray > k, stop adding indexes to currentArray and push the "currentArray" into array of "eligibleArrays".
         for (let j = 0; j < a.length; j++) {
             if (i != j) {
                 if (subarraySum + a[j] <= k) {
@@ -26,9 +20,6 @@ const longestSubarray = (a, k) => {
                 }
             }
         }
-        eligibleArrays.push(currentArray);
-        //Once currentArray is pushed into eligibleArrays, check to see if currentArray's length is greater than the length of 
-        //any of the prior eligibleArrays. If so, update maximumLength to the length of the currentArray.
         if (currentArray.length > maximumLength) {
             maximumLength = currentArray.length;
         };
